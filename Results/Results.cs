96f69ff15992e6ed3baa99723ad49d8308221ec8
Luegiu Yuusha-sama <cassianofpereira@gmail.com>
2025-08-31 16:43:44 -0300

    Adicionando o recurso UserDTOResponse

--- Results/Results.cs
+++ Results/Results.cs
@@ -1,3 +1,4 @@
+using newapi.domain.dtos;
 using newapi.domain.entities;
 
 namespace newapi.results;
@@ -7,17 +8,34 @@ public class UserResult
     public UserResult(bool Success, List<string?> Error, User? User)
     {
         this.Success = Success;
-        this.Errors = Error;
+        Errors = Error;
         this.User = User;
     }
     public UserResult(bool Success, List<string?> Error)
     {
         this.Success = Success;
-        this.Errors = Error;
+        Errors = Error;
     }
+
+    public UserResult(bool Success, List<string?> Error, List<User> users)
+    {
+        this.Success = Success;
+        Errors = Error;
+        Users = users;
+    }
+
+    public UserResult(bool Success, List<string?> Error, List<UserDTOResponse> usersResponse)
+    {
+        this.Success = Success;
+        Errors = Error;
+        UsersResponse = usersResponse;
+    }
+
     public bool Success = default!;
-    private readonly List<string?> Errors;
+    private readonly List<string?> Errors = [];
     public User? User { get; private set; }
+    public List<User> Users { get; private set; } = [];
+    public List<UserDTOResponse> UsersResponse { get; private set; } = [];
 
     public List<string?> GetErrors()
     {
