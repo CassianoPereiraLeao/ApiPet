d8b8c40d07db4043fed0b8c95f0f1daad998f236
Luegiu Yuusha-sama <cassianofpereira@gmail.com>
2025-08-31 16:45:43 -0300

    Adicionando o recurso de swagger

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using newapi.domain.services;
 using newapi.infra.data;
-using newapi.infra.interfaces;
+using newapi.domain.interfaces;
 using newapi.routes;
+using newapi.infra.interfaces;
+using newapi.infra.repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("Default"));
@@ -15,14 +21,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UserRoutes();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UserRoutes();
 
 app.Run();
